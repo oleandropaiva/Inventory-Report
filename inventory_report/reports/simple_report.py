15c9d090ff657712f7b6345ae6333261387b6edc
Leandro Paiva <leandroapaiva@yahoo.com>
2023-04-05 20:17:54 -0300

    ✔ Feat: Gerar a versão simplificada do relatório

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,4 +1,5 @@
 from collections import Counter
+from datetime import datetime, date
 
 
 class SimpleReport:
@@ -7,18 +8,25 @@ class SimpleReport:
         datas_de_fabricacao = []
         for product in list_of_products:
             datas_de_fabricacao.append(product["data_de_fabricacao"])
-        print(datas_de_fabricacao)
+
+        data_mais_antiga = min(datas_de_fabricacao)
 
         datas_de_validade = []
         for product in list_of_products:
             datas_de_validade.append(product["data_de_validade"])
+        datas_datetime = [
+            datetime.strptime(data, "%Y-%m-%d") for data in datas_de_validade
+        ]
+        date_now = date.today()
+
+        data_mais_recente = min(
+            datas_datetime, key=lambda x: abs((x.date() - date_now).days)
+        )
 
         empresas_mais_produtos = []
         for product in list_of_products:
             empresas_mais_produtos.append(product["nome_da_empresa"])
 
-        data_mais_antiga = min(datas_de_fabricacao)
-        data_mais_recente = min(datas_de_validade)
         nome_da_empresa = Counter(empresas_mais_produtos).most_common()[0][0]
 
         return (
