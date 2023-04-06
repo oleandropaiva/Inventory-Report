bc9224808b963c84339c977f5282e8fb10bc8124
Leandro Paiva <leandroapaiva@yahoo.com>
2023-04-06 20:57:54 -0300

    ✔ feat: Gera relatórios através de um arquivo JSON

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,6 +1,7 @@
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 import csv
+import json
 
 
 class Inventory:
@@ -15,5 +16,9 @@ class Inventory:
 
     @staticmethod
     def file_read(path):
-        with open(path, "r") as file:
-            return list(csv.DictReader(file))
+        if path.endswith(".csv"):
+            with open(path, "r") as file:
+                return list(csv.DictReader(file))
+        elif path.endswith(".json"):
+            with open(path, "r") as file:
+                return json.load(file)
