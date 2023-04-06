932f3665a4cd298629bd8c5d2e796eac93b92439
Leandro Paiva <leandroapaiva@yahoo.com>
2023-04-05 21:14:34 -0300

    ✔ Feat: Gera a versão completa do relatório

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,2 +1,19 @@
-class CompleteReport:
-    pass
+from inventory_report.reports.simple_report import SimpleReport
+from collections import Counter
+
+
+class CompleteReport(SimpleReport):
+    @classmethod
+    def generate(cls, list_of_products):
+        report = super().generate(list_of_products)
+        report += "\nProdutos estocados por empresa:\n"
+
+        empresas = []
+        for product in list_of_products:
+            empresas.append(product["nome_da_empresa"])
+
+        estoque = Counter(empresas).most_common()
+        for empresa, quantidade in estoque:
+            report += f"- {empresa}: {quantidade}\n"
+
+        return report
